daa47d7188098709dfffdfa6607028e75b699287
yelouafi <yelouafi@gmail.com>
2014-05-30 14:02:07 +0100

    added recipes;

--- README.md
+++ README.md
@@ -6,4 +6,50 @@ work was based upon the [massive-js][massive] library. massivejs is a nodejs mod
 
 [massive]: https://github.com/robconery/massive-js
 
-basically, litesql intends to brings the same interface into sqlite database. but actually it provides some enhancements, so instead of writing something like
+basically, litesql intends to brings the same interface into sqlite database. undeway the module use the [sqlite3][sqlite3]
+
+[sqlite3]: https://github.com/mapbox/node-sqlite3
+
+## Basic usage
+
+install it
+```
+npm install litesql
+```
+
+then use it
+```javascript
+var litesql = require('litesql');
+var db = litesql.db(':memory:');
+
+db.serialize(function() {
+    db.createTable(
+        'todos',                                        // table name
+        {                                               // table defintion
+            id: 'pk',                                   // shortcut for id INTEGER PRIMARY KEY AUTOINCREMENT
+            task: { type: 'text', required: true },     // you can pass it also 'unique: true'
+            duedate: 'date',
+            completed: 'boolean'                        // type alias is managed internally 
+        }
+    ).run(); 
+    /*
+        you can also write
+        var query = db.createTable(...);
+        query.run( function (err) { ... } );
+    */
+    
+    // helper class
+    var todos = new litesql.Table('todos', 'id', db);
+    
+    for(var i = 1; i <= 10; i++) {
+        todos.insert({ task: 'Task #'+i, duedate: new Date(), completed: false }).run();
+    }
+    
+    todos.find().all( function(err, tasks){
+        assert.equal(err, null);
+        assert.equal(tasks.length, 10);
+        done();
+    });
+    
+})
+```
--- test/test.js
+++ test/test.js
@@ -438,6 +438,39 @@ describe('litesql', function(){
             }
         }
         
+        it('should create a table and insert data into it', function(done) {
+            db.serialize(function() {
+                db.createTable(
+                    'todos',        // table name
+                    {               // table defintion
+                        id: 'pk',   // shortcut for id INTEGER PRIMARY KEY AUTOINCREMENT
+                        task: { type: 'text', required: true }, // you can pass it also 'unique: true'
+                        duedate: 'date',
+                        completed: 'boolean'  // types alias are managed internally 
+                    }
+                ).run(); 
+                /*
+                    you can also write
+                    var query = db.createTable(...);
+                    query.run( function (err) { ... } );
+                */
+                
+                // helper class
+                var todos = new litesql.Table('todos', 'id', db);
+                
+                for(var i = 1; i <= 10; i++) {
+                    todos.insert({ task: 'Task #'+i, duedate: new Date(), completed: false }).run();
+                }
+                
+                todos.find().all( function(err, tasks){
+                    assert.equal(err, null);
+                    assert.equal(tasks.length, 10);
+                    done();
+                });
+                
+            })
+        });
+        
         it( "should upgrade model", function(done) {	
 			
             utils.waterfall([
@@ -474,6 +507,8 @@ describe('litesql', function(){
                     done();
             });                                   
         });
+        
+        
     })
 
 });
